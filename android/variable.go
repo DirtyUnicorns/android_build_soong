eb2809083d2cb90b004c2d4debbb5e315a7558c9
Nick Reuter <nreuter85@gmail.com>
2017-08-22 18:11:35 -0500

    soong: add Has_legacy_mmap product variable
    Change-Id: I743eeac6ec40e356fdeb2e8c4f8957c09e84b434

--- android/variable.go
+++ android/variable.go
@@ -118,6 +118,10 @@ type variableProperties struct {
 		Needs_text_relocations struct {
 			Cppflags []string
 		}
+
+		Has_legacy_mmap struct {
+			Cppflags []string
+		}
 	} `android:"arch_variant"`
 }
 
@@ -237,6 +241,7 @@ type productVariables struct {
 	VendorVars map[string]map[string]string `json:",omitempty"`
 
 	Needs_text_relocations     *bool `json:",omitempty"`
+	Has_legacy_mmap            *bool `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {
